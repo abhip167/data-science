4feb39f8a63e2726b3a259e5ce3a011675761c63
abhip167 <abhishekpatel167@gmail.com>
2023-05-09 11:14:17 -0400

    Added CSV export and extra fields to the table

--- src/Pages/UserData.jsx
+++ src/Pages/UserData.jsx
@@ -1,49 +1,98 @@
 import { useEffect, useState, useMemo } from "react";
+
+import { Box, Button } from "@mui/material";
+import { FileDownload } from "@mui/icons-material";
+
 import AxiosModule from "../State/Axios.js";
 import MaterialReactTable from "material-react-table";
 
-const SUCCESS_MESSAGE = "Receipient added successfully";
-const ERROR_MESSAGE = "Receipient already exists";
-const DELETE_MESSAGE = "Receipient deleted successfully";
+import { ExportToCsv } from "export-to-csv";
+
+const columns = [
+  {
+    accessorKey: "name",
+    header: "Name",
+    size: 200, //decrease the width of this column
+  },
+  {
+    accessorKey: "organization",
+    header: "Organization",
+    size: 100, //decrease the width of this column
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+    size: 100, //decrease the width of this column
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone",
+    size: 100, //decrease the width of this column
+  },
+  {
+    accessorKey: "nature_of_work",
+    header: "Nature of work",
+    size: 300, //decrease the width of this column
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+    size: 300, //decrease the width of this column
+  },
+  {
+    accessorKey: "timestamp",
+    header: "Created At",
+  },
+  {
+    accessorKey: "collectionMethod",
+    header: " Collection Method",
+    size: 200,
+  },
+  {
+    accessorKey: "format",
+    header: "Data Fromat",
+    size: 200,
+  },
+  {
+    accessorKey: "updateFrequency",
+    header: "Data Update Frequency",
+    size: 200,
+  },
+  {
+    accessorKey: "ethicsApproval",
+    header: "Ethics Approval",
+    size: 200,
+  },
+  {
+    accessorKey: "collaboration",
+    header: "Collaboration Availablity",
+    size: 200,
+  },
+  {
+    accessorKey: "financialSupport",
+    header: "Financial Support ",
+    size: 200,
+  },
+];
+
+const csvOptions = {
+  fieldSeparator: ",",
+  quoteStrings: '"',
+  decimalSeparator: ".",
+  showLabels: true,
+  useBom: true,
+  useKeysAsHeaders: false,
+  headers: columns.map((c) => c.header),
+};
+
+const csvExporter = new ExportToCsv(csvOptions);
 
 export default () => {
   const [recepients, setRecepients] = useState([]);
 
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: "name",
-        header: "Name", //uses the default width from defaultColumn prop
-      },
-      {
-        accessorKey: "organization",
-        header: "Organization",
-      },
-      {
-        accessorKey: "email",
-        header: "Email",
-      },
-      {
-        accessorKey: "phone",
-        header: "Phone",
-      },
-      {
-        accessorKey: "nature_of_work",
-        header: "Nature of work",
-        size: 300, //decrease the width of this column
-      },
-      {
-        accessorKey: "description",
-        header: "Description",
-        size: 300, //decrease the width of this column
-      },
-      {
-        accessorKey: "timestamp",
-        header: "Created At",
-      },
-    ],
-    []
-  );
+  const handleExportData = () => {
+    csvExporter.generateCsv(recepients);
+  };
 
   const fetchRecepients = async () => {
     const response = await AxiosModule.get("details");
@@ -68,6 +117,21 @@ export default () => {
       }}
       enableColumnResizing
       columnResizeMode="onChange" //default is "onEnd"
+      renderTopToolbarCustomActions={({ table }) => (
+        <Box
+          sx={{ display: "flex", gap: "1rem", p: "0.5rem", flexWrap: "wrap" }}
+        >
+          <Button
+            color="primary"
+            //export all data that is currently in the table (ignore pagination, sorting, filtering, etc.)
+            onClick={handleExportData}
+            startIcon={<FileDownload />}
+            variant="contained"
+          >
+            Export All Data
+          </Button>
+        </Box>
+      )}
     />
   );
 };
